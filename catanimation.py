00bc7d1181efeca6c3981406d137bdbe6602c16c
Colton Hill <chill@coltons-mbp.dhcp.fnal.gov>
2016-10-20 15:14:50 -0500

    simple animated cat picture, found through tutorial manual

---
+++ catanimation.py
@@ -0,0 +1,50 @@
+import pygame, sys
+from pygame.locals import *
+
+pygame.init()
+
+FPS = 30
+fpsClock = pygame.time.Clock()
+
+#setup window
+DISPLAYSURF = pygame.display.set_mode((400, 300), 0, 32)
+pygame.display.set_caption('Animation')
+
+WHITE = (255, 255, 255)
+catImg = pygame.image.load('cat.png')
+catx = 10
+caty = 10
+direction = 'right'
+
+#main loop
+while True:
+	DISPLAYSURF.fill(WHITE)
+
+	if direction == 'right':
+		catx += 5
+		if catx == 280:
+			direction = 'down'
+	#remember that pixels increase to the right and down!!!
+	elif direction == 'down':
+		caty += 5
+		if caty == 220:
+			direction = 'left'
+	elif direction == 'left':
+		catx -= 5
+		if catx == 10:
+			direction = 'up'
+	elif direciton == 'up':
+		caty -= 5
+		if caty == 10:
+			direction = 'right'
+
+	DISPLAYSURF.blit(catImg, (catx, caty))
+
+	for event in pygame.event.get():
+		if event.type == QUIT:
+			pygame.quit()
+			sys.exit()
+
+	pygame.display.update()
+
+	fpsClock.tick(FPS)
