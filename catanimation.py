f703d6ae72b26c3457deb398c09eec987be39fd5
Colton Hill <chill@coltons-mbp.dhcp.fnal.gov>
2016-10-22 16:48:31 -0500

    now changes colours

--- catanimation.py
+++ catanimation.py
@@ -1,4 +1,5 @@
-import pygame, sys
+import pygame
+import sys
 from pygame.locals import *
 
 pygame.init()
@@ -6,45 +7,62 @@ pygame.init()
 FPS = 30
 fpsClock = pygame.time.Clock()
 
-#setup window
+# setup window
 DISPLAYSURF = pygame.display.set_mode((400, 300), 0, 32)
 pygame.display.set_caption('Animation')
 
 WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+BLUE = (0, 0, 255)
+BLACK = (255, 255, 255)
+
 catImg = pygame.image.load('cat.png')
 catx = 10
 caty = 10
 direction = 'right'
 
-#main loop
+DISPLAYSURF.fill(WHITE)
+
+colour = WHITE
+
+# main loop
 while True:
-	DISPLAYSURF.fill(WHITE)
+    DISPLAYSURF.fill(colour)
 
-	if direction == 'right':
-		catx += 5
-		if catx == 280:
-			direction = 'down'
-	#remember that pixels increase to the right and down!!!
-	elif direction == 'down':
-		caty += 5
-		if caty == 220:
-			direction = 'left'
-	elif direction == 'left':
-		catx -= 5
-		if catx == 10:
-			direction = 'up'
-	elif direciton == 'up':
-		caty -= 5
-		if caty == 10:
-			direction = 'right'
+    if direction == 'right':
+        catx += 5
+        if catx == 280:
+            direction = 'down'
+            DISPLAYSURF.fill(BLUE)
+            colour = BLUE
+    # remember that pixels increase to the right and down!!!
+    elif direction == 'down':
+        caty += 5
+        if caty == 220:
+            direction = 'left'
+            DISPLAYSURF.fill(RED)
+            colour = RED
+    elif direction == 'left':
+        catx -= 5
+        if catx == 10:
+            direction = 'up'
+            DISPLAYSURF.fill(BLACK)
+            colour = BLACK
+    elif direction == 'up':
+        caty -= 5
+        if caty == 10:
+            direction = 'right'
+            DISPLAYSURF.fill(WHITE)
+            colour = WHITE
 
-	DISPLAYSURF.blit(catImg, (catx, caty))
+    DISPLAYSURF.blit(catImg, (catx, caty))
 
-	for event in pygame.event.get():
-		if event.type == QUIT:
-			pygame.quit()
-			sys.exit()
+    for event in pygame.event.get():
+        if event.type == QUIT:
+            pygame.quit()
+            sys.exit()
 
-	pygame.display.update()
+    pygame.display.update()
 
-	fpsClock.tick(FPS)
+    fpsClock.tick(FPS)
